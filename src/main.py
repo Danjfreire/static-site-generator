eb8033394407463b9fc3c4d39a9f9039a18ef63a
Danjfreire <danjfreire@gmail.com>
2025-08-21 20:43:31 -0300

    feat: serving with github pages

--- src/main.py
+++ src/main.py
@@ -2,13 +2,18 @@ from textnode import TextNode, TextType
 from file_util import copy_files
 from markdown_blocks import markdown_to_html_node, extract_markdown_title
 import os
+import sys
 
 def main():
-    copy_files("static", "public")
-    generate_pages_recursevely("content", "template.html", "public")
+    path = sys.argv[1]
+    basepath = path if not None else "/"
 
 
-def generate_pages_recursevely(dir_path_content, template_path, dest_dir_path):
+    copy_files("static", "docs")
+    generate_pages_recursevely("content", "template.html", "docs", basepath)
+
+
+def generate_pages_recursevely(dir_path_content, template_path, dest_dir_path, basepath):
     if not os.path.exists(dir_path_content):
         raise Exception(f"Invalid directory : {dir_path_content}")
     
@@ -18,12 +23,12 @@ def generate_pages_recursevely(dir_path_content, template_path, dest_dir_path):
         file_path = os.path.join(dir_path_content, filename)
         if os.path.isfile(file_path):
             dest_path = os.path.join(dest_dir_path, filename.replace(".md", ".html"))
-            generate_page(file_path, template_path, dest_path)
+            generate_page(file_path, template_path, dest_path, basepath)
         elif os.path.isdir(file_path):
-            generate_pages_recursevely(file_path, template_path, os.path.join(dest_dir_path, filename))
+            generate_pages_recursevely(file_path, template_path, os.path.join(dest_dir_path, filename), basepath)
         
 
-def generate_page(from_path: str, template_path: str, dest_path: str):
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath:str):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     if not os.path.exists(from_path):
@@ -41,6 +46,9 @@ def generate_page(from_path: str, template_path: str, dest_path: str):
     html = markdown_to_html_node(markdown_content).to_html()
     title = extract_markdown_title(markdown_content)
     filled_template = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    filled_template = filled_template.replace('href="/', f'href="{basepath}')
+    filled_template = filled_template.replace('src="/', f'src="{basepath}')
+
 
     dest_dir = os.path.dirname(dest_path)
     if dest_dir and not os.path.exists(dest_dir):
